00231383b9e75fc311736718298b759652f60d4c
Shuhao Liu <shuhao@ece.toronto.edu>
2022-03-07 15:19:14 +0800

    Cleaned up third party dependencies.

---
+++ dependencies.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+CDIR="$(pwd)"
+echo "Current Directory "
+echo $CDIR
+
+echo "Dependency Packages"
+case "$OSTYPE" in
+  darwin*)
+    brew install folly
+    ;;
+  linux*)
+    sudo apt-get install -y g++ curl git cmake build-essential autoconf libtool
+    ;;
+esac
+
+# Download third party header libraries.
+# Third party libraries include:
+#   - boost-ext/sml v1.1.4 https://boost-ext.github.io/sml/
+echo "Updating Submodules"
+cd $CDIR
+git submodule update --init --recursive
