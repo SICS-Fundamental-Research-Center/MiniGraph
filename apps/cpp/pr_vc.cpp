67240fda37e7f9f051d9348cc207b9e522dfcc73
hsiaoko <zhuxk@buaa.edu.cn>
2022-09-29 22:01:23 +0800

    Update PR

--- apps/cpp/pr_vc.cpp
+++ apps/cpp/pr_vc.cpp
@@ -267,7 +267,7 @@ class PRPIE : public minigraph::AutoAppBase<GRAPH_T, CONTEXT_T> {
 
 struct Context {
   size_t num_iter = 5;
-  float epsilon = 0.0001;
+  float epsilon = 0.000001;
   float gamma = 0.01;
 };
 
