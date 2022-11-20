faed054ab10049303a196024db716befd076599c
hsiaoko <zhuxk@buaa.edu.cn>
2022-11-20 22:16:24 +0800

    Update PageRank

--- apps/cpp/pr_gc_frontier.cpp
+++ apps/cpp/pr_gc_frontier.cpp
@@ -197,7 +197,7 @@ class PRPIE : public minigraph::AutoAppBase<GRAPH_T, CONTEXT_T> {
 
 struct Context {
   size_t num_iter = 3;
-  float epsilon = 0.0000001;
+  float epsilon = 0.001;
   float gamma = 0.01;
 };
 
