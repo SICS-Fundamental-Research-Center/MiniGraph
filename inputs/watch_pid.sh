2e146093d44094329179b451df8960ee72cfface
hsiaoko <zhuxk@buaa.edu.cn>
2023-01-30 16:18:01 +0800

    Clearn up

--- inputs/watch_pid.sh
+++
@@ -1,3 +0,0 @@
-while ps -p 297472 --no-headers --format "etime pid %cpu %mem rss"; do
-       sleep 1
-done
--- inputs/watch_pid_io.sh
+++
@@ -1,4 +0,0 @@
-while pidstat -d -p 211204 1 --no-headers
-    sudo iotop -p 193852 --no-headers --format "etime pid %cpu %mem rss"; do
-       sleep 1
-done
