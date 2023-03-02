fb0047fd63b1ed655beffe70074badad41c2de47
hsiaoko <zhuxk@buaa.edu.cn>
2023-03-02 21:32:15 +0800

    let type convert function perform in parallel.

--- tools/types_convert.cpp
+++ tools/types_convert.cpp
@@ -1,6 +1,5 @@
 #include <gflags/gflags.h>
 #include <sys/stat.h>
-
 #include <iostream>
 #include <string>
 
@@ -10,6 +9,7 @@
 #include "utility/io/data_mngr.h"
 #include "utility/io/edge_list_io_adapter.h"
 #include "utility/paritioner/partitioner_base.h"
+#include "utility/thread_pool.h"
 
 using CSR_T = minigraph::graphs::ImmutableCSR<gid_t, vid_t, vdata_t, edata_t>;
 using GRAPH_BASE_T = minigraph::graphs::Graph<gid_t, vid_t, vdata_t, edata_t>;
@@ -17,10 +17,17 @@ using EDGE_LIST_T = minigraph::graphs::EdgeList<gid_t, vid_t, vdata_t, edata_t>;
 using EDGE_LIST_UNSIGNED =
     minigraph::graphs::EdgeList<gid_t, unsigned, vdata_t, edata_t>;
 
-EDGE_LIST_UNSIGNED* VID_T2unsigned(EDGE_LIST_T* in_edgelist_graph) {
+EDGE_LIST_UNSIGNED* VID_T2unsigned(EDGE_LIST_T* in_edgelist_graph,
+                                   const size_t cores) {
   LOG_INFO("VID_T to unsigned.");
-  auto out_edgelist_graph = new EDGE_LIST_UNSIGNED;
 
+  auto thread_pool = minigraph::utility::CPUThreadPool(cores, 1);
+  std::mutex mtx;
+  std::condition_variable finish_cv;
+  std::unique_lock<std::mutex> lck(mtx);
+  std::atomic<size_t> pending_packages(cores);
+
+  auto out_edgelist_graph = new EDGE_LIST_UNSIGNED;
   out_edgelist_graph->gid_ = in_edgelist_graph->get_gid();
   out_edgelist_graph->num_edges_ = in_edgelist_graph->get_num_edges();
   out_edgelist_graph->num_vertexes_ = in_edgelist_graph->get_num_vertexes();
@@ -31,22 +38,50 @@ EDGE_LIST_UNSIGNED* VID_T2unsigned(EDGE_LIST_T* in_edgelist_graph) {
   out_edgelist_graph->globalid_by_localid_ = (unsigned*)malloc(
       sizeof(unsigned) * out_edgelist_graph->get_num_vertexes());
 
-  out_edgelist_graph->buf_graph_ =
-      (unsigned*)malloc(sizeof(unsigned) * out_edgelist_graph->num_edges_ * 2);
-  for (size_t i = 0; i < out_edgelist_graph->get_num_edges(); i++) {
-    out_edgelist_graph->buf_graph_[i * 2] =
-        in_edgelist_graph->buf_graph_[i * 2];
-    out_edgelist_graph->buf_graph_[i * 2 + 1] =
-        in_edgelist_graph->buf_graph_[i * 2 + 1];
-  }
+  out_edgelist_graph->buf_graph_ = (unsigned*)malloc(
+      sizeof(unsigned) * out_edgelist_graph->get_num_edges() * 2);
 
-  for (size_t i = 0; i < out_edgelist_graph->get_num_vertexes(); i++) {
-    out_edgelist_graph->vdata_[i] = in_edgelist_graph->vdata_[i];
-    out_edgelist_graph->globalid_by_localid_[i] =
-        in_edgelist_graph->globalid_by_localid_[i];
-  }
+  memset(out_edgelist_graph->buf_graph_, 0,
+         sizeof(unsigned) * out_edgelist_graph->get_num_edges() * 2);
 
-  LOG_INFO("Convert any VID_T to unsigned");
+  LOG_INFO("Convert edges.");
+  pending_packages.store(cores);
+  for (size_t i = 0; i < cores; i++) {
+    size_t tid = i;
+    thread_pool.Commit([tid, &cores, &out_edgelist_graph, &in_edgelist_graph,
+                        &pending_packages, &finish_cv]() {
+      for (size_t j = tid; j < out_edgelist_graph->get_num_edges();
+           j += cores) {
+        out_edgelist_graph->buf_graph_[j * 2] =
+            in_edgelist_graph->buf_graph_[j * 2];
+        out_edgelist_graph->buf_graph_[j * 2 + 1] =
+            in_edgelist_graph->buf_graph_[j * 2 + 1];
+      }
+      if (pending_packages.fetch_sub(1) == 1) finish_cv.notify_all();
+      return;
+    });
+  }
+  finish_cv.wait(lck, [&] { return pending_packages.load() == 0; });
+
+  LOG_INFO("Convert vertexes.");
+  pending_packages.store(cores);
+  for (size_t i = 0; i < cores; i++) {
+    size_t tid = i;
+    thread_pool.Commit([tid, &cores, &out_edgelist_graph, &in_edgelist_graph,
+                        &pending_packages, &finish_cv]() {
+      for (size_t j = tid; j < out_edgelist_graph->get_num_vertexes();
+           j += cores) {
+        out_edgelist_graph->vdata_[j] = in_edgelist_graph->vdata_[j];
+        out_edgelist_graph->globalid_by_localid_[j] =
+            in_edgelist_graph->globalid_by_localid_[j];
+      }
+      if (pending_packages.fetch_sub(1) == 1) finish_cv.notify_all();
+      return;
+    });
+  }
+  finish_cv.wait(lck, [&] { return pending_packages.load() == 0; });
+
+  LOG_INFO("Convert VID_T for all vertexes and edges to unsigned");
   return out_edgelist_graph;
 }
 
@@ -69,7 +104,7 @@ void TypeConvert(std::string src_pt, std::string dst_pt, std::size_t cores,
   std::string dst_meta_pt = dst_pt + "minigraph_meta" + ".bin";
   std::string dst_data_pt = dst_pt + "minigraph_data" + ".bin";
   std::string dst_vdata_pt = dst_pt + "minigraph_vdata" + ".bin";
-  auto out_edgelist_graph = VID_T2unsigned(edgelist_graph);
+  auto out_edgelist_graph = VID_T2unsigned(edgelist_graph, cores);
 
   std::ofstream meta_file(dst_meta_pt, std::ios::binary | std::ios::app);
   std::ofstream data_file(dst_data_pt, std::ios::binary | std::ios::app);
