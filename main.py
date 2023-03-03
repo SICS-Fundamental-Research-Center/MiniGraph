c6c4ffb21f406d26d2909bbd0c036f74b13244a5
liuyang <1844061236@buaa.edu.cn>
2023-03-03 04:11:55 +0000

    fix

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ from preprocessing import getWebsk
 from torch.utils.data import Dataset, DataLoader
 from load import getDataLoader
 import argparse
+import numpy as np
+import random
+
+def setup_seed(seed):
+     torch.manual_seed(seed)
+     torch.cuda.manual_seed_all(seed)
+     np.random.seed(seed)
+     random.seed(seed)
+     torch.backends.cudnn.deterministic = True
+# 设置随机数种子
+setup_seed(20)
 
 class TimeSeriesDataSet(Dataset):
   """
