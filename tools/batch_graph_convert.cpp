ee370ecf2cbc2dcb874e15ecb60814d90af3ec14
hsiaoko <zhuxk@buaa.edu.cn>
2023-06-20 10:43:05 +0800

    minor changes.

--- tools/batch_graph_convert.cpp
+++ tools/batch_graph_convert.cpp
@@ -1,14 +1,12 @@
-#include <sys/stat.h>
-#include <iostream>
-#include <string>
-
-#include <gflags/gflags.h>
-
 #include "graphs/edgelist.h"
 #include "graphs/immutable_csr.h"
 #include "portability/sys_data_structure.h"
 #include "portability/sys_types.h"
 #include "utility/io/edge_list_io_adapter.h"
+#include <gflags/gflags.h>
+#include <sys/stat.h>
+#include <iostream>
+#include <string>
 
 using CSR_T = minigraph::graphs::ImmutableCSR<gid_t, vid_t, vdata_t, edata_t>;
 using GRAPH_BASE_T = minigraph::graphs::Graph<gid_t, vid_t, vdata_t, edata_t>;
@@ -33,24 +31,6 @@ void BatchEdgeList2EdgeList(std::string src_pt, std::string dst_pt,
   edge_list_io_adapter.Write(*graph, edgelist_bin, meta_pt, data_pt, vdata_pt);
 }
 
-void EdgeListCSV2EdgeListBin(std::string src_pt, std::string dst_pt,
-                             char separator_params = ',') {
-  std::cout << " #Converting " << FLAGS_t << ": input: " << src_pt
-            << " output: " << dst_pt << std::endl;
-  minigraph::utility::io::EdgeListIOAdapter<gid_t, vid_t, vdata_t, edata_t>
-      edge_list_io_adapter;
-  auto graph = new EDGE_LIST_T;
-  std::string meta_pt = dst_pt + "minigraph_meta" + ".bin";
-  std::string data_pt = dst_pt + "minigraph_data" + ".bin";
-  std::string vdata_pt = dst_pt + "minigraph_vdata" + ".bin";
-  LOG_INFO("Write: ", meta_pt);
-  LOG_INFO("Write: ", data_pt);
-  LOG_INFO("Write: ", vdata_pt);
-  edge_list_io_adapter.Read((GRAPH_BASE_T*)graph, edgelist_csv,
-                            separator_params, 0, src_pt);
-  edge_list_io_adapter.Write(*graph, edgelist_bin, meta_pt, data_pt, vdata_pt);
-}
-
 int main(int argc, char* argv[]) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
 
@@ -61,10 +41,9 @@ int main(int argc, char* argv[]) {
   std::size_t cores = FLAGS_cores;
   std::string graph_type = FLAGS_t;
 
-  if (FLAGS_tobin) {
-    if (graph_type == "edgelist_bin") {
+  if (FLAGS_tobin && FLAGS_frombin == false && FLAGS_in_type == "edgelist" &&
+      FLAGS_out_type == "edgelist") {
       BatchEdgeList2EdgeList(src_pt, dst_pt, *FLAGS_sep.c_str(), cores);
-    }
   }
   gflags::ShutDownCommandLineFlags();
 }
