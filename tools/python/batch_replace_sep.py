8d0bce49a8a8ba60c5eba53cc906bcb07d861db8
hsiaoko <zhuxk@buaa.edu.cn>
2023-06-21 14:27:10 +0800

    minor change.

--- tools/python/batch_replace_sep.py
+++ tools/python/batch_replace_sep.py
@@ -14,14 +14,17 @@ print("#### Input path: ", in_dir_pt, " Output path:", out_dir_pt, " ####")
 list_dir = listdir(in_dir_pt)
 print(list_dir)
 
+final_data = pd.DataFrame()
 for file_name in list_dir:
     in_pt = in_dir_pt + file_name
     out_pt = out_dir_pt + file_name
     print(file_name)
     print(in_pt)
+    print(out_pt)
     data = pd.read_csv(in_pt, sep="\t")
     print(data)
     data.to_csv(out_pt, sep = " ", index=0)
+    concact[final_data,data, axis=1]
 """
 
 """
