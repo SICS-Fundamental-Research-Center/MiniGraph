582cd7b86f85a4e9c33a3d7728fed0a994b66ff8
xiaoke.zhu@outlook.com <hsiaoko>
2023-06-26 14:42:59 +0800

    minor changes.

--- apps/cpp/coloring.cpp
+++ apps/cpp/coloring.cpp
@@ -89,7 +89,6 @@ class ColoringPIE : public minigraph::AutoAppBase<GRAPH_T, CONTEXT_T> {
     LOG_INFO("PEval() - Processing gid: ", graph.gid_,
              " num_vertexes: ", graph.get_num_vertexes());
 
-    graph.ShowGraph();
     auto start_time = std::chrono::system_clock::now();
     Bitmap* in_visited = new Bitmap(graph.get_num_vertexes());
     Bitmap* out_visited = new Bitmap(graph.get_num_vertexes());
