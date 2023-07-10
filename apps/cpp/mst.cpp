a04129e002ad9403eea88c022101adc92d5a228f
xiaoke.zhu@outlook.com <hsiaoko>
2023-07-10 23:47:38 +0800

    Update mst.

--- apps/cpp/mst.cpp
+++ apps/cpp/mst.cpp
@@ -19,18 +19,26 @@ class MST {
   size_t* offset_ = nullptr;
   VID_T* buffer_ = nullptr;
   size_t curr_offset_ = 0;
+  std::ofstream* file_ = nullptr;
+  size_t num_vertexes_ = 0;
 
  public:
-  MST(size_t num_vertexes) {
-    buffer_ = (VID_T*)malloc(sizeof(VID_T) * 3 *
+  MST(size_t num_vertexes, std::string file_path) {
+    LOG_INFO("MST(): num_vertexes - ", num_vertexes, " path - ", file_path);
+
+    buffer_ = (VID_T*)malloc(sizeof(VID_T) * 2 *
                              ceil(num_vertexes / ALIGNMENT_FACTOR) *
                              ALIGNMENT_FACTOR);
+    memset(buffer_, 0,
+           sizeof(VID_T) * 2 * ceil(num_vertexes / ALIGNMENT_FACTOR) *
+               ALIGNMENT_FACTOR);
     offset_ =
         (size_t*)malloc(sizeof(size_t) * ceil(num_vertexes / ALIGNMENT_FACTOR) *
                         ALIGNMENT_FACTOR);
     memset(offset_, 0,
            sizeof(size_t) * ceil(num_vertexes / ALIGNMENT_FACTOR) *
                ALIGNMENT_FACTOR);
+    file_ = new std::ofstream(file_path, std::ios::binary | std::ios::app);
     return;
   }
 
@@ -42,16 +50,10 @@ class MST {
 
   // Append an edge with minimum-weight outgoing edge.
   void Append(const VID_T& src, const VID_T& dst) {
-    if (offset_[dst] != 0 &&
-        *(buffer_ + 2 * sizeof(VID_T) * offset_[dst]) != src) {
-      // change spanning tree structure by set original val to src.
-      // LOG_INFO(*(buffer_ + 2 * sizeof(VID_T) * offset_[dst]), "->", dst,
-      //         " change to ", src, "->", dst);
-      *(buffer_ + 2 * sizeof(VID_T) * offset_[dst]) = src;
-    } else if (*(buffer_ + 2 * sizeof(VID_T) * offset_[dst]) == src &&
-               *(buffer_ + 2 * sizeof(VID_T) * offset_[dst] + 1) == dst) {
+    //if(curr_offset_ > num_vertexes_) return;
+    if (*(buffer_ + 2 * sizeof(VID_T) * offset_[dst]) == src &&
+        *(buffer_ + 2 * sizeof(VID_T) * offset_[dst] + 1) == dst) {
     } else {
-      // LOG_INFO("append", src, "->", dst);
       auto local_offset = __sync_fetch_and_add(&curr_offset_, 1);
       offset_[dst] = local_offset;
       *(buffer_ + 2 * sizeof(VID_T) * local_offset) = src;
@@ -66,6 +68,14 @@ class MST {
                *(buffer_ + 2 * sizeof(VID_T) * i + 1));
     }
     return;
+  }
+
+  void Write() {
+    file_->write((char*)buffer_, sizeof(VID_T) * 2 * curr_offset_);
+    curr_offset_ = 0;
+    memset(buffer_, 0,
+           sizeof(VID_T) * 2 * ceil(num_vertexes_ / ALIGNMENT_FACTOR) *
+               ALIGNMENT_FACTOR);
   }
 
   const inline size_t get_num_edges() { return curr_offset_; }
@@ -118,19 +128,17 @@ class MSTAutoMap : public minigraph::AutoMapBase<GRAPH_T, CONTEXT_T> {
   // Find and update moe for each component (fragment);
   static void kernel_choose_minimum(GRAPH_T* graph, const size_t tid,
                                     Bitmap* visited, const size_t step,
-                                    Bitmap* in_visited, size_t* active_vertex,
+                                    Bitmap* in_visited, VID_T* vid_map,
                                     VDATA_T* vdata, VID_T* moe,
                                     EDATA_T* fragment_moe_val) {
     for (size_t i = tid; i < graph->get_num_vertexes(); i += step) {
-      auto u = graph->GetVertexByIndex(i);
-      for (size_t j = 0; j < u.indegree; j++) {
-        if (vdata[u.in_edges[j]] == vdata[graph->localid2globalid(i)]) continue;
-        if (write_min(fragment_moe_val + vdata[graph->localid2globalid(i)],
-                      u.edata[j])) {
-          moe[graph->localid2globalid(i)] = u.in_edges[j];
-          // LOG_INFO(" vid: ", graph->localid2globalid(i),
-          //          " moe: ", moe[graph->localid2globalid(i)]);
-          in_visited->set_bit(graph->localid2globalid(i));
+      auto u_root_id = vdata[graph->localid2globalid(i)];
+      auto u_root = graph->GetVertexByIndex(vid_map[u_root_id]);
+      for (size_t j = 0; j < u_root.indegree; j++) {
+        if (vdata[u_root.in_edges[j]] >= u_root_id) continue;
+        if (write_min(fragment_moe_val + vdata[u_root_id], u_root.edata[j])) {
+          moe[u_root_id] = u_root.in_edges[j];
+          in_visited->set_bit(u_root_id);
         }
       }
     }
@@ -199,7 +207,7 @@ class MSTPIE : public minigraph::AutoAppBase<GRAPH_T, CONTEXT_T> {
       memset(fragment_moe_val_, 1,
              sizeof(EDATA_T) * this->msg_mngr_->get_max_vid());
 
-      mst_ = new MST(this->msg_mngr_->get_max_vid());
+      mst_ = new MST(this->msg_mngr_->get_max_vid(), this->context_.file_path);
       this->auto_map_->template ParallelDo(
           task_runner, MSTAutoMap<GRAPH_T, CONTEXT_T>::kernel_global_init,
           this->msg_mngr_->get_max_vid(), this->msg_mngr_->GetGlobalVdata(),
@@ -228,19 +236,16 @@ class MSTPIE : public minigraph::AutoAppBase<GRAPH_T, CONTEXT_T> {
     visited.clear();
     size_t num_new_edges = 0;
     auto vdata = this->msg_mngr_->GetGlobalVdata();
+    auto vid_map = this->msg_mngr_->GetVidMap();
     while (active_vertexes != 0) {
       in_visited->clear();
       active_vertexes = 0;
-      LOG_INFO("Find moe.");
       this->auto_map_->ActiveMap(
           graph, task_runner, &visited,
           MSTAutoMap<GRAPH_T, CONTEXT_T>::kernel_choose_minimum, in_visited,
-          &active_vertexes, this->msg_mngr_->GetGlobalVdata(), moe_,
-          fragment_moe_val_);
+          vid_map, this->msg_mngr_->GetGlobalVdata(), moe_, fragment_moe_val_);
       size_t niters = 0;
-      LOG_INFO("Append edges.");
       while (!in_visited->empty()) {
-        LOG_INFO("niters: ", niters++);
         this->auto_map_->ParallelDo(
             task_runner, MSTAutoMap<GRAPH_T, CONTEXT_T>::kernel_label_prop,
             this->msg_mngr_->get_max_vid(), in_visited, out_visited,
@@ -250,6 +255,8 @@ class MSTPIE : public minigraph::AutoAppBase<GRAPH_T, CONTEXT_T> {
         std::swap(in_visited, out_visited);
         out_visited->clear();
       }
+      memset(fragment_moe_val_, 1,
+             sizeof(EDATA_T) * this->msg_mngr_->get_max_vid());
       LOG_INFO("#");
     }
 
@@ -272,14 +279,14 @@ class MSTPIE : public minigraph::AutoAppBase<GRAPH_T, CONTEXT_T> {
     out_visited->clear();
     visited.clear();
     size_t num_new_edges = 0;
+    auto vid_map = this->msg_mngr_->GetVidMap();
     while (active_vertexes != 0) {
       in_visited->clear();
       active_vertexes = 0;
       this->auto_map_->ActiveMap(
           graph, task_runner, &visited,
           MSTAutoMap<GRAPH_T, CONTEXT_T>::kernel_choose_minimum, in_visited,
-          &active_vertexes, this->msg_mngr_->GetGlobalVdata(), moe_,
-          fragment_moe_val_);
+          vid_map, this->msg_mngr_->GetGlobalVdata(), moe_, fragment_moe_val_);
 
       while (!in_visited->empty()) {
         this->auto_map_->ParallelDo(
@@ -291,6 +298,7 @@ class MSTPIE : public minigraph::AutoAppBase<GRAPH_T, CONTEXT_T> {
         std::swap(in_visited, out_visited);
         out_visited->clear();
       }
+      mst_->Write();
     }
     // this->mst_->ShowMST();
     delete in_visited;
@@ -306,6 +314,7 @@ class MSTPIE : public minigraph::AutoAppBase<GRAPH_T, CONTEXT_T> {
 
 struct Context {
   int t = 0;
+  std::string file_path;
 };
 
 using CSR_T = minigraph::graphs::ImmutableCSR<gid_t, vid_t, vdata_t, edata_t>;
@@ -321,6 +330,7 @@ int main(int argc, char* argv[]) {
   size_t buffer_size = FLAGS_buffer_size;
 
   Context context;
+  context.file_path = work_space + "output.bin";
 
   auto mst_auto_map = new MSTAutoMap<CSR_T, Context>();
   auto mst_pie = new MSTPIE<CSR_T, Context>(mst_auto_map, context);
